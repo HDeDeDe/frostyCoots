41c776d7196338038cf4597e595ec741a4252a27
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2023-02-21 18:24:16 -0800

    moved halve to PVTools

--- Assets/Scripts/PVTools.cs
+++ Assets/Scripts/PVTools.cs
@@ -19,6 +19,8 @@ public static class PVTools
     public const float crimpLimitNegative = -0.000001f;
     public readonly static Vector2 overlapBox = new(0.225f, 0.0625f);
 
+    public static float Halve(float input) { return input / 2f;}
+    public static Vector2 Halve(Vector2 input) { return input / 2f;}
     public static Vector2 CapSpeed(Vector2 input)
     {
         return new(Math.Clamp(input.x, velocityLimitNegative, velocityLimit), Math.Clamp(input.y, velocityLimitNegative, velocityLimit));
